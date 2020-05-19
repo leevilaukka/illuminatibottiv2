ce8c7984a093db589f2bfbcf501bff9cc8101ae6
Leevi Laukka <leevi.laukka@gmail.com>
2020-05-19 16:03:29 +0200

    Added feature.js command

---
+++ commands/feature.js
@@ -0,0 +1,103 @@
+const argsToString = require("../helpers/argsToString");
+const axios = require("axios");
+
+module.exports = {
+    name: "feature",
+    description: "Ehdota uutta ominaisuutta tai ilmoita bugista",
+    aliases: ["report","ilmoita"],
+    cooldown: 15,
+    execute(message, args) {
+        // List of supported types (issue labels)
+        const typelist = [
+            "bug","enhancement","invalid","question"
+        ];
+        // Github API authentication object
+        const password = process.env.GITHUB_KEY;
+        const auth = {
+            username: "leevilaukka",
+            password
+        };
+        // Variables from arguments
+        const typearg = args[0];
+        const desc = argsToString(args.slice(1)) + "\n\n > Tämä ilmoitus on lähetetty IlluminatiBotin kautta.";
+        // Title parsing
+        let title = desc + "";
+        title = title.substr(0,20) + "..";
+
+        // Check if typelist includes given type
+        if(!typelist.includes(typearg)){
+            // Check if typearg is a number
+            if (!isNaN(typearg) && !desc) {
+                // Get Github API with issue number
+                axios.get(`https://api.github.com/repos/leevilaukka/illuminatibottiv2/issues/${typearg}`, {
+                    auth
+                })
+                    .then(res => {
+                        // Create embed from returned data
+                        const embed = {
+                            title: res.data.title,
+                            description: res.data.body,
+                            url: res.data.html_url,
+                            color: `0x${res.data.labels[0].color}`,
+                            fields: [
+                                {
+                                    name: "Tila",
+                                    value: res.data.state
+                                },
+                                {
+                                    name: "Tägi",
+                                    value: res.data.labels[0].name
+                                },
+                                {
+                                    name: "Numero",
+                                    value: res.data.number
+                                }
+                            ],
+                            timestamp: res.data.updated_at
+                        };
+                        // Send embed and catch Axios errors
+                        message.channel.send({embed})
+                })
+                    .catch(() => message.channel.send("Ilmoitusta ei löytynyt."))
+            }
+            // If typelist doesn't include given type send message
+            else return message.channel.send(`Tuo ei ole oikeantyyppinen ilmoitus! Oikeat tyypit ovat ${typelist.toString()}`)
+        }
+        // Init data for Axios POST
+        const data = {
+            title,
+            body: desc,
+            labels: [typearg]
+        };
+        // If desc given and type is not a number, post the data to Github API
+        if(desc && isNaN(typearg)){
+        axios.post("https://api.github.com/repos/leevilaukka/illuminatibottiv2/issues", data, {
+            auth
+        })
+            .then(res => {
+                console.log(res.data);
+                // Create embed from returned data
+                const embed = {
+                        title: "Uusi ilmoitus luotu!",
+                        description: res.data.body,
+                        url: res.data.html_url,
+                        color: `0x${res.data.labels[0].color}`,
+                        fields: [
+                            {
+                                name: "Tägi",
+                                value: res.data.labels[0].name
+                            },
+                            {
+                                name: "Numero",
+                                value: res.data.number
+                            }
+                        ],
+                        timestamp:Date.now()
+                    };
+                // Send embed and catch Axios errors
+                message.channel.send("Voit tarkistaa ilmoituksen tilan kirjoittamalla ilmoituksen numeron komennon ainoaksi argumentiksi",{embed})
+            })
+            .catch(e => message.channel.send("Tapahtui virhe: " + e.message))
+        }
+    }
+};
