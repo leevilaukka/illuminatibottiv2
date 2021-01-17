6a02bc498aea73bfde407c3daeb3e09f17fe37b0
Leevi Laukka <leevi.laukka@gmail.com>
2021-01-17 23:21:35 +0200

    bruh updated

--- commands/bruh.js
+++ commands/bruh.js
@@ -2,6 +2,7 @@ module.exports = {
   name: "bruh",
   description: "bruh",
   async execute(message, args, settings, client) {
+    message.delete({timeout: 1000});
     await client.playFile(message, "assets/bruh.mp3");
   },
 };
