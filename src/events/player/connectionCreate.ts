7eb546e6b6aaaf15790601b6a5f0ccb147e7334a
Leevi Laukka <leevi.laukka@gmail.com>
2023-05-07 06:31:26 +0300

    finally fix music

--- src/events/player/connectionCreate.ts
+++ src/events/player/connectionCreate.ts
@@ -4,6 +4,21 @@ import { PlayerEvent } from "../../types/PlayerEvent";
 const evt: PlayerEvent = (client, queue, connection) => {
     if (!(connection instanceof StreamDispatcher)) return;
     queue.metadata.channel.send(`Liitytään kanavalle...`);
+
+    // Fix for voice connection not being destroyed
+    queue.connection.voiceConnection.on('stateChange', (oldState, newState) => {
+        const oldNetworking = Reflect.get(oldState, 'networking');
+        const newNetworking = Reflect.get(newState, 'networking');
+
+        const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
+            const newUdp = Reflect.get(newNetworkState, 'udp');
+            clearInterval(newUdp?.keepAliveInterval);
+        }
+
+        oldNetworking?.off('stateChange', networkStateChangeHandler);
+        newNetworking?.on('stateChange', networkStateChangeHandler);
+    });
 }
+
 
 export default evt;
